8653160968280306636b60c85adbc1d68f18fc61
okamoto <sakie.okamoto@rakus-partners.co.jp>
2026-02-12 11:32:29 +0900

    SQL

---
+++ sql-sakie.okamoto/answer.sql
@@ -0,0 +1,27 @@
+問1
+    INSERT INTO items (id, category_id, name, price)VALUES
+    (13, 1, '鰯', 150),
+    (14, 2, '羊', 650);
+
+問2
+    UPDATE items
+    SET price=price-(price*0.1);
+
+問3
+    DELETE FROM employees
+    WHERE end_date<='2013-03-31';
+
+問4
+    SELECT * FROM employees
+    WHERE start_date='2000-01-01' ;
+
+問5
+
+
+問6
+
+
+問7
+
+
+問8
