d75dff346e24d8eea2489f6adc98c89c8bdec894
okamoto0930 <sakie.okamoto@rakus-partners.co.jp>
2026-03-02 12:19:18 +0900

    Springロジック

---
+++ spring-exam/spring-exam/exam-web-spring-thymeleaf/src/main/java/com/example/q6/ParamController.java
@@ -0,0 +1,30 @@
+package com.example.q6;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * リクエストパラメータ<code>linkTo</code>の値を見て、フォーワード先を切り替えなさい。
+ * 以下のようになるようにしなさい。
+ * <pre>
+ * linkTo = "1"の時、遷移先は6/first.html。
+ * linkTo = "2"の時、遷移先は6/second.html。
+ * 1,2以外の場合は、RuntimeExceptionをthrowしてください。
+ * </pre>
+ */
+@Controller
+@RequestMapping("")
+public class ParamController {
+
+	@RequestMapping("/q61")
+	public String gotoq6(){
+		
+			return "first";
+	}
+
+	@RequestMapping("/q62")
+	public String gotoq66(){
+		
+			return "second";
+	}
+}
